0abde3b73d572b46898395a0320abc951e221577
Harsha <harsha.xg@gmail.com>
2015-11-13 13:09:50 +0530

    Better landing page

--- app/src/main/java/com/cameraomr/android/LandingActivity.java
+++ app/src/main/java/com/cameraomr/android/LandingActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
